969755e9345d0d50732bff4ba1b5777f86d42d98
Stephen Roebuck <shroebuck@gmail.com>
2021-11-13 00:28:37 -0500

    Initial
    Website Backbone

---
+++ js/js_custom.js
@@ -0,0 +1,342 @@
+;
+(function($, window, document, undefined) {
+    'use strict';
+    var $winW = function() {
+        return $(window).width();
+    };
+    var $winH = function() {
+        return $(window).height();
+    };
+    var $screensize = function(element) {
+        $(element).width($winW()).height($winH());
+    };
+    var screencheck = function(mediasize) {
+        if (typeof window.matchMedia !== "undefined") {
+            var screensize = window.matchMedia("(max-width:" + mediasize + "px)");
+            if (screensize.matches) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if ($winW() <= mediasize) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+    $(document).ready(function() {
+        $(window).on('load', function() {
+            $('.preloader').fadeOut();
+            $('.animated-row').each(function() {
+                var $this = $(this);
+                $this.find('.animate').each(function(i) {
+                    var $item = $(this);
+                    var animation = $item.data('animate');
+                    $item.on('inview', function(event, isInView) {
+                        if (isInView) {
+                            setTimeout(function() {
+                                $item.addClass('animated ' + animation).removeClass('animate');
+                            }, i * 50);
+                        } else if (!screencheck(767)) {
+                            $item.removeClass('animated ' + animation).addClass('animate');
+                        }
+                    });
+                });
+            });
+        });
+        if ($('.facts-list').length) {
+            $('.facts-list').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 3,
+                margin: 30,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 2,
+                        margin: 20
+                    },
+                    992: {
+                        items: 3,
+                        margin: 30
+                    }
+                }
+            });
+        }
+        if ($('.services-list').length) {
+            $('.services-list').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 3,
+                margin: 30,
+                autoplay: false,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    460: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 2,
+                        margin: 20
+                    },
+                    992: {
+                        items: 3,
+                        margin: 30
+                    }
+                }
+            });
+        }
+        if ($('.gallery-list').length) {
+            $('.gallery-list').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 3,
+                autoplay: true,
+                smartSpeed: 1000,
+                autoplayTimeout: 4000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    576: {
+                        items: 2,
+                        margin: 20
+                    },
+                    992: {
+                        items: 3,
+                        margin: 30
+                    }
+                }
+            });
+        }
+        if ($('.testimonials-slider').length) {
+            $('.testimonials-slider').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                items: 1,
+                margin: 30,
+                autoplay: true,
+                smartSpeed: 700,
+                autoplayTimeout: 6000,
+                responsive: {
+                    0: {
+                        items: 1,
+                        margin: 0
+                    },
+                    768: {
+                        items: 1
+                    }
+                }
+            });
+        }
+        if ($('.fullpage-default').length) {
+            var myFullpage = new fullpage('.fullpage-default', {
+                licenseKey: ' C7F41B00-5E824594-9A5EFB99-B556A3D5',
+                anchors: ['home', 'about', 'prev-work', 'pricing', 'terms-of-service', 'contact', 'slide07'],
+                menu: '#nav',
+                lazyLoad: true,
+                navigation: true,
+                navigationPosition: 'right',
+                scrollOverflow: true,
+                responsiveWidth: 768,
+                responsiveHeight: 600,
+                responsiveSlides: true
+            });
+        }
+        $(document).on('click', '.navbar-toggle', function() {
+            $('.navbar-collapse').slideToggle(300);
+            return false;
+        }).on('click', '.navigation-menu > li > a', function() {
+            $('.navbar-collapse').slideUp(300);
+        }).on('click', '.next-section', function() {
+            fullpage_api.moveSectionDown();
+        });
+        $('.facts-row').on('inview', function(event, isInView) {
+            $('.count-number').each(function() {
+                $(this).prop('Counter', 0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 1000,
+                    easing: 'swing',
+                    step: function(now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+                setTimeout(function() {
+                    $('.count-number').removeClass('count-number').addClass('counted');
+                }, 1000);
+            });
+        });
+        $('.skills-row').on('inview', function(event, isInView) {
+            $(this).addClass('view');
+        });
+        $(document).on('click', '.menu-trigger', function() {
+            $('body').toggleClass('sidemenu-open');
+        }).on('click', '.side-menu .navbar-nav li a', function() {
+            $('body').removeClass('sidemenu-open');
+        });
+    });
+
+
+
+
+
+})(jQuery, window, document);
+
+const Logo = {
+    settings : {
+        hidden: false
+    },
+
+    hideLogo: function(content) {
+        // content.classList.add("hidden");
+        console.log("hide logo");
+    },
+
+    showLogo: function(content) {
+        console.log("show logo");
+        // content.classList.remove("hidden");
+    },
+
+    init: function(el) {
+        el.addEventListener("wheel", 
+            function(e) {
+                if(document.body.classList.contains("home")) {
+                    showLogo(el);
+                } else {
+                    hideLogo(el);
+                }
+            });
+    }
+};
+
+const Accordion = {
+    settings: {
+      // Expand the first item by default
+      first_expanded: false,
+      // Allow items to be toggled independently
+      toggle: false
+    },
+
+    openAccordion: function(toggle, content) {
+      if (content.children.length) {
+        toggle.classList.add("is-open");
+        let final_height = Math.floor(content.children[0].offsetHeight);
+        content.style.height = final_height + "px";
+      }
+    },
+
+    closeAccordion: function(toggle, content) {
+      toggle.classList.remove("is-open");
+      content.style.height = 0;
+    },
+
+    init: function(el) {
+      const _this = this;
+
+      // Override default settings with classes
+      let is_first_expanded = _this.settings.first_expanded;
+      if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
+      let is_toggle = _this.settings.toggle;
+      if (el.classList.contains("is-toggle")) is_toggle = true;
+
+      // Loop through the accordion's sections and set up the click behavior
+      const sections = el.getElementsByClassName("accordion");
+      const all_toggles = el.getElementsByClassName("accordion-head");
+      const all_contents = el.getElementsByClassName("accordion-body");
+      for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+        const toggle = all_toggles[i];
+        const content = all_contents[i];
+
+        // Click behavior
+        toggle.addEventListener("click", function(e) {
+          if (!is_toggle) {
+            // Hide all content areas first
+            for (let a = 0; a < all_contents.length; a++) {
+              _this.closeAccordion(all_toggles[a], all_contents[a]);
+            }
+
+            // Expand the clicked item
+            _this.openAccordion(toggle, content);
+          } else {
+            // Toggle the clicked item
+            if (toggle.classList.contains("is-open")) {
+              _this.closeAccordion(toggle, content);
+            } else {
+              _this.openAccordion(toggle, content);
+            }
+          }
+        });
+
+        // Expand the first item
+        if (i === 0 && is_first_expanded) {
+          _this.openAccordion(toggle, content);
+        }
+      }
+    }
+  };
+
+  (function() {
+    // Initiate all instances on the page
+    const accordions = document.getElementsByClassName("accordions");
+    for (let i = 0; i < accordions.length; i++) {
+      Accordion.init(accordions[i]);
+    }
+
+    const logos = document.getElementById("logo_hide");
+
+    Logo.init(logos);
+  })();
+
+
+
+  // Select the node that will be observed for mutations
+const logoNode = document.getElementById("logo_hide");
+
+const targetNode = document.getElementById('bdy');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: true, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(const mutation of mutationsList) {
+        if (mutation.attributeName == 'class') {
+            let str = targetNode.classList.value;
+
+            if(str.includes("home")) logoNode.style.opacity = '0';
+            else logoNode.style.opacity = '1';
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
